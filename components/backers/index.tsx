1b3b583a165f87c9fe789e3ae606dcdbb5e988d2
tinnd <tinndev21@gmail.com>
2024-03-23 13:07:48 +0700

    fix

--- components/backers/index.tsx
+++ components/backers/index.tsx
@@ -16,22 +16,22 @@ const Backer = () => {
                     <button>Backers</button>
                 </Box>
                 <Heading className='mt-6 text-[48px] font-bold' text='Backers' />
-                <div className='hidden lg:flex items-center gap-10 mt-6 w-[647px] mx-auto'>
-                    <div className='w-[196px] h-[196px] relative'>
+                <div className='hidden lg:flex items-center justify-between gap-10 mt-6 w-[1172px] mx-auto'>
+                    <div className='w-[346px] h-[196px] relative'>
                         <div className='relative flex flex-col gap-4 items-center justify-center w-full h-full'>
                             <Image src={icon1} alt='icon1' />
                             <p className='title text-xl font-medium'>AZCOINVEST</p>
                         </div>
                         <Image src={backer} alt='backer' fill priority />
                     </div>
-                    <div className='w-[196px] h-[196px] relative'>
+                    <div className='w-[346px] h-[196px] relative'>
                         <div className='relative flex flex-col gap-4  items-center justify-center w-full h-full '>
                             <Image src={icon2} alt='icon1' />
                             <p className='title text-xl font-medium'>RL</p>
                         </div>
                         <Image src={backer} alt='backer' fill priority />
                     </div>
-                    <div className='w-[196px] h-[196px] relative'>
+                    <div className='w-[346px] h-[196px] relative'>
                         <div className='relative flex flex-col gap-4 items-center justify-center w-full h-full '>
                             <Image src={icon3} alt='icon1' />
                             <p className='title text-xl font-medium'>DUCK CRYPTO</p>
